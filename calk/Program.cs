2d4b0de731d3200a747c554f7b991f9ed0603be3
Drei4 <antipin-lukinf@mail.ru>
2022-08-07 21:20:40 +0300

    add coment in calk

--- calk/Program.cs
+++ calk/Program.cs
@@ -1,13 +1,13 @@
 ﻿Console.WriteLine("Введите первое число: "); //ввод первого числа
 double a = Convert.ToDouble(Console.ReadLine()); 
 
-Console.WriteLine("Введите второе число число: "); //ввод первого числа
+Console.WriteLine("Введите второе число число: "); //ввод второго числа
 double b = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Знак математической операции: "); //ввод первого числа
+Console.WriteLine("Знак математической операции: "); //ввод знака мат. операции
 string znak = Convert.ToString(Console.ReadLine());
 {
-    
+
 if(znak == "/")
 {
     Console.Write("Ответ: ");
