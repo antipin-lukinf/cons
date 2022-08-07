e1fa6bef76e59a68207b49e5dc3200e5979dac5c
Drei4 <antipin-lukinf@mail.ru>
2022-08-07 22:15:44 +0300

    add calk v1.1

--- calk/Program.cs
+++ calk/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите первое число: "); //ввод первого числа
-double a = Convert.ToDouble(Console.ReadLine()); 
+double a = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Введите второе число число: "); //ввод второго числа
 double b = Convert.ToDouble(Console.ReadLine());
@@ -8,29 +8,37 @@ Console.WriteLine("Знак математической операции: "); /
 string znak = Convert.ToString(Console.ReadLine());
 {
 
-if(znak == "/")
-{
-    Console.Write("Ответ: ");
-    Console.WriteLine(a/b);
-}
-if(znak == "*")
-{
-    Console.Write("Ответ: ");
-    Console.WriteLine(a*b);
-}
-if(znak == "+")
-{
-    Console.Write("Ответ: ");
-    Console.WriteLine(a+b);
-}
-if(znak == "-")
-{
-    Console.Write("Ответ: ");
-    Console.WriteLine(a-b);
-}
+    if (znak == "/")
+    {
+        Console.Write("Ответ: ");
+        Console.WriteLine(a / b);
+
+    }
+    else
+    if (znak == "*")
+    {
+        Console.Write("Ответ: ");
+        Console.WriteLine(a * b);
+
+    }
+    else
+    if (znak == "+")
+    {
+        Console.Write("Ответ: ");
+        Console.WriteLine(a + b);
+
+    }
+    else
+    if (znak == "-")
+    {
+        Console.Write("Ответ: ");
+        Console.WriteLine(a - b);
+
+    }
     else
     {
         Console.Write("Ответ: Введен не корректный знак операции ");
-   
     }
+
+
 }
