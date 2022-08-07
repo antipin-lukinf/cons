b67adab2f38d1e0b3647f0d35c5ab33d95c3c0e6
Drei4 <antipin-lukinf@mail.ru>
2022-08-07 21:17:07 +0300

    add calk

---
+++ calk/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите первое число: "); //ввод первого числа
+double a = Convert.ToDouble(Console.ReadLine()); 
+
+Console.WriteLine("Введите второе число число: "); //ввод первого числа
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Знак математической операции: "); //ввод первого числа
+string znak = Convert.ToString(Console.ReadLine());
+{
+    
+if(znak == "/")
+{
+    Console.Write("Ответ: ");
+    Console.WriteLine(a/b);
+}
+if(znak == "*")
+{
+    Console.Write("Ответ: ");
+    Console.WriteLine(a*b);
+}
+if(znak == "+")
+{
+    Console.Write("Ответ: ");
+    Console.WriteLine(a+b);
+}
+if(znak == "-")
+{
+    Console.Write("Ответ: ");
+    Console.WriteLine(a-b);
+}
+    else
+    {
+        Console.Write("Ответ: Введен не корректный знак операции ");
+   
+    }
+}
