6470488685d11ad8aec5285d3fd9a98432caa799
Drei4 <antipin-lukinf@mail.ru>
2022-08-14 21:25:49 +0300

    add Method sort massiv

--- Ex012_Methods/Program.cs
+++ Ex012_Methods/Program.cs
@@ -1,11 +1,53 @@
 ﻿// Таблица умножения
 
-for ( int i = 2; i <= 10; i++)
+/*for ( int i = 2; i <= 10; i++)
 {
     for (int j = 2; j <= 10; j++)
     {
         Console.WriteLine($"{i} x {j} = {i * j}");
     }
     Console.WriteLine();
+
+}*/
+
+int[] arr = { 1, 5, 4, 7, 6, 1, 9, 7, 8 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+             if (array[j] < array[minPosition])            //поиск максимального эл-та
+              minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    
+
+    }
+
+
+    
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
 
 }
