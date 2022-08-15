bf82dfa1b8ecfb2b507c5690ac7e64c805f2bfe1
Drei4 <antipin-lukinf@mail.ru>
2022-08-15 12:57:42 +0300

    end Ex012

--- Ex012/Program.cs
+++ Ex012/Program.cs
@@ -11,5 +11,29 @@ void PrintArray(int[] array)
     Console.Write("");
 }
 
+void SelectionSort(int[] array)
+{
+    //int count = array.Length;
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i+1; j < array.Length ; j++)
+        {
+            if(array[j] < array[minPosition])
+                minPosition = j;
+        }
+        int temporay = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporay;
+    }
+    Console.WriteLine(" ");
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
 
 PrintArray(arr);
