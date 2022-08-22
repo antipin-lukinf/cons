09a41b9e589103e1c3ece751b0ec856c04ec4b72
Drei4 <antipin-lukinf@mail.ru>
2022-08-22 16:53:48 +0300

    add Ex013

---
+++ seminar4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespase ConsoleApp1
+{   
+     class Program
+    {
+        static void ReadNumber(int number)   //создание функции
+        {
+            Console.WriteLine(number * number);
+        }
+        static string WriteNumber(int number)
+        {
+            return number % 2 == 0 ? "Кратное" : "Не кратное";
+        }
+        /*static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+             ReadNumber(number);     
+             Console.WriteLine(WriteNumber(number));
+        }
+    }*/
+    }
+}
